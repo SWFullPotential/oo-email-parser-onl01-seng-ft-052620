a179b581f68e755ab600cec15e591f5fec969c26
Samantha Wabi <swfullpotential@gmail.com>
2020-06-05 22:12:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,10 +9,4 @@ def initialize(emails)
   @emails = emails
 end 
 
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
-
 end
